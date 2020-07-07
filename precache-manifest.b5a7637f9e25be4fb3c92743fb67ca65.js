d9d9c2c1389f6e04b94883affabfc0399fa0c0cc
callalishykh <62282919+callalishykh@users.noreply.github.com>
2020-07-07 16:36:01 +0500

    Updates

---
+++ precache-manifest.b5a7637f9e25be4fb3c92743fb67ca65.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c3f4b15b540a8bf225bb683d481825e",
+    "url": "/covid-map/index.html"
+  },
+  {
+    "revision": "2bc8dcfdbd9177c41652",
+    "url": "/covid-map/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "8bdea8a65e757b7864c5",
+    "url": "/covid-map/static/css/main.2df0e671.chunk.css"
+  },
+  {
+    "revision": "2bc8dcfdbd9177c41652",
+    "url": "/covid-map/static/js/2.366a876f.chunk.js"
+  },
+  {
+    "revision": "0fb0e672f1308cb5b7677cdb93fc42ab",
+    "url": "/covid-map/static/js/2.366a876f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bdea8a65e757b7864c5",
+    "url": "/covid-map/static/js/main.8b0ad033.chunk.js"
+  },
+  {
+    "revision": "3fe5a03dd07576c991b5",
+    "url": "/covid-map/static/js/runtime-main.f29636fb.js"
+  },
+  {
+    "revision": "d9dc09a779e9b8e1499dc68932c031c0",
+    "url": "/covid-map/static/media/pin.d9dc09a7.png"
+  }
+]);
